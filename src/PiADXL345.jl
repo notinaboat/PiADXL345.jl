e08bc432577a397b922c0b021bc6de4b2c7aa02e
notinaboat <notinaboat@icloud.com>
2020-11-30 14:24:12 +1100

    add adxl_set_offset, convert output to unit: g

--- src/PiADXL345.jl
+++ src/PiADXL345.jl
@@ -16,7 +16,7 @@ end
 Base.setindex!(p::ActiveLowPin, v) = p.pin[] = iszero(v) ? 1 : 0
 Base.getindex(p::ActiveLowPin) = iszero(p.pin[])
 
-BBSPI.delay(s::BBSPI.SPISlave) = PiGPIOMEM.spin(10)
+BBSPI.delay(s::BBSPI.SPISlave) = PiGPIOMEM.spin(50)
 
 
 struct ADXL345{T} <: AbstractChannel{Vector{UInt16}}
@@ -62,6 +62,16 @@ adxl_is_connected(spi) = all(adxl_read(spi, 0) .== 0xE5) # DEVID [1, p15]
 
 adxl_enable(spi) = adxl_write(spi, 0x2D, 0b00001000) # POWER_CTL [1, p16]
 
+adxl_offset(spi) = [adxl_read(spi, 0x1E), # OFSX [1, p15]
+                    adxl_read(spi, 0x1F), # OFSY [1, p15]
+                    adxl_read(spi, 0x20)] # OFSZ [1, p15]
+
+function adxl_set_offset(spi, x, y, z)
+    adxl_write(spi, 0x1E, unsigned(Int8(x))) # OFSX [1, p15]
+    adxl_write(spi, 0x1F, unsigned(Int8(y))) # OFSY [1, p15]
+    adxl_write(spi, 0x20, unsigned(Int8(z))) # OFSZ [1, p15]
+end
+
 adxl_bw_rate_12hz5(spi) = adxl_write(spi, 0x2C, 0b00000111) # BW_RATE [1, p16]
 adxl_bw_rate_25hz(spi)  = adxl_write(spi, 0x2C, 0b00001000) # BW_RATE [1, p16]
 adxl_bw_rate_50hz(spi)  = adxl_write(spi, 0x2C, 0b00001001) # BW_RATE [1, p16]
@@ -76,7 +86,7 @@ Read `n` bytes from `address`.
 function adxl_read(spi, address, n=1)
     n += 1
     cin=zeros(UInt8, n+1, BBSPI.output_width(spi))
-    cout = [0b11000000 | address] # SPI header [1, p9 Figure 5]
+    cout = UInt8[0b11000000 | address] # SPI header [1, p9 Figure 5]
     BBSPI.transfer(spi, cout, cin)
     return view(cin, 2:n, :)
 end
@@ -88,7 +98,7 @@ end
 Write byte `v` to `address`.
 """
 function adxl_write(spi, address, v)
-    cout = UInt8[address, UInt8(v)] # SPI header [1, p9 Figure 5]
+    cout = UInt8[address, v] # SPI header [1, p9 Figure 5]
     BBSPI.transfer(spi, cout)
     nothing
 end
@@ -105,7 +115,7 @@ function Base.take!(adxl::ADXL345)
     x = @. signed(v[1,:] | UInt16(v[2,:]) << 8)
     y = @. signed(v[3,:] | UInt16(v[4,:]) << 8)
     z = @. signed(v[5,:] | UInt16(v[6,:]) << 8)
-    vcat(permutedims.([x, y, z])...)
+    vcat(permutedims.([x/256, y/256, z/256])...) # 0.0039g/LSB [1, Table 1, p3]
 end
 
 
